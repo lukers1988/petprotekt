8f115475ee6e0f5f0422411ff1a28971699ad531
Łukasz Gadzina <l.gadzina@dookpro>
2024-07-03 09:32:31 +0200

    logowanie google

--- src/component/auth/Callback.tsx
+++ src/component/auth/Callback.tsx
@@ -5,12 +5,14 @@ import { useLocation, useNavigate } from "react-router-dom";
 const Callback = () => {
     const location = useLocation();
     const navigate = useNavigate();
+    const search = window.location.search;
+    const params = new URLSearchParams(search);
+    const code = params.get('code');
 
     useEffect(() => {
         const fetchUser = async() => {
             try {
-                await customAxios.get('/auth/login/google/callback').then((response: any): void => {
-                    console.log('User data: ', response.data);
+                await customAxios.get(`/auth/login/google/callback?code=${code}`).then((response: any): void => {
 
                     localStorage.setItem('token', response.data.token);
 
