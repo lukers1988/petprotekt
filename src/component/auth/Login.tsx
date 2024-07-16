61993f12e61820b0de8bbbc53919fef3be039eb1
Łukasz Gadzina <l.gadzina@dookpro>
2024-07-16 11:55:29 +0200

    pobieranie linku google

--- src/component/auth/Login.tsx
+++ src/component/auth/Login.tsx
@@ -33,7 +33,7 @@ const Login = () => {
         dispatch(loginStart());
 
         try {
-            customAxios.get('/auth/login/google').then((response: any) => {
+            customAxios.get('/auth/login/google/').then((response: any) => {
                 console.log(response)
                 // window.location.href = response.data.google_url;
             });
