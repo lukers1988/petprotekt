7aed21f5822dd8b0177e61073b8560f75f06d63e
Adrian Kubeczko <adrian.kubeczko@gmail.com>
2024-08-20 15:40:13 +0200

    fixed language picker

--- src/component/common/LanguagePicker.tsx
+++ src/component/common/LanguagePicker.tsx
@@ -1,48 +1,48 @@
 import { DropdownButton } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
-import languages from '@appConst/languages';
+import languages, { fallbackLanguage } from '@appConst/languages';
 import DropdownItemMobile from '@appComponents/styled/DropdownItemMobile';
 
 const LanguagePicker = () => {
-    const { t, i18n } = useTranslation();
-    const chosenLanguage = languages.find((language) => language.id === i18n.language)!;
-    console.log('LANGUAGE DETECTION: ', i18n.language);
+  const { t, i18n } = useTranslation();
+  const chosenLanguage =
+    languages.find((language) => language.id === i18n.language) || fallbackLanguage;
 
-    return (
-        <DropdownButton
-            title={
-                <img
-                    style={{
-                        height: 20,
-                        width: 40
-                    }}
-                    src={chosenLanguage.flag}
-                />
-            }
-            size="sm"
-        >
-            {languages.map((language) => {
-                return (
-                    <DropdownItemMobile
-                        $breakpoint={430}
-                        onClick={() => {
-                            i18n.changeLanguage(language.id);
-                        }}
-                    >
-                        <img
-                            style={{
-                                height: 20,
-                                width: 40,
-                                marginRight: 10
-                            }}
-                            src={language.flag}
-                        />
-                        {t(`languages:${language.id}`)}
-                    </DropdownItemMobile>
-                );
-            })}
-        </DropdownButton>
-    );
+  return (
+    <DropdownButton
+      title={
+        <img
+          style={{
+            height: 20,
+            width: 40
+          }}
+          src={chosenLanguage.flag}
+        />
+      }
+      size="sm"
+    >
+      {languages.map((language) => {
+        return (
+          <DropdownItemMobile
+            $breakpoint={430}
+            onClick={() => {
+              i18n.changeLanguage(language.id);
+            }}
+          >
+            <img
+              style={{
+                height: 20,
+                width: 40,
+                marginRight: 10
+              }}
+              src={language.flag}
+            />
+            {t(`languages:${language.id}`)}
+          </DropdownItemMobile>
+        );
+      })}
+    </DropdownButton>
+  );
 };
 
 export default LanguagePicker;
--- src/const/languages.ts
+++ src/const/languages.ts
@@ -1,15 +1,17 @@
 import flagPl from '@appImages/flags/pl.png';
 import flagEn from '@appImages/flags/en.png';
 
+export const fallbackLanguage = {
+  id: 'en-En',
+  flag: flagEn
+};
+
 const languages = [
-    {
-        id: 'pl-PL',
-        flag: flagPl
-    },
-    {
-        id: 'en-En',
-        flag: flagEn
-    }
+  {
+    id: 'pl-PL',
+    flag: flagPl
+  },
+  fallbackLanguage
 ];
 
 export default languages;
